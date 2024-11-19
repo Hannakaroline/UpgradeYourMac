1bb6ba42f90db5e26b2cf81b61d6d0752ceb92b2
Hanna Torales Palacios <hanna@Hannas-MacBook-Pro.local>
2024-11-19 16:46:21 +0900

    project iniciated

---
+++ UpgradeYourMac/UpgradeYourMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpgradeYourMacApp.swift
+//  UpgradeYourMac
+//
+//  Created by Hanna Torales Palacios on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct UpgradeYourMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
